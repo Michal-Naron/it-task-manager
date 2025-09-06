ebd4f76603ca038e56a1a32a8ff1c13c0397c781
Michal-Naron <michanaron@gmail.com>
2025-09-06 15:53:49 +0200

    feat(models): add initial models for TaskType, Position, Worker, and Task

--- workflows/models.py
+++ workflows/models.py
@@ -1,3 +1,71 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    NAME_CHOICES = [
+        ('bug', 'Bug'),
+        ('new_feature', 'New feature'),
+        ('breaking_change', 'Breaking change'),
+        ('refactoring', 'Refactoring'),
+        ('qa',"QA")
+    ]
+    name = models.CharField(
+        max_length=255,
+        choices=NAME_CHOICES,
+        default='new_feature'
+    )
+
+    def __str__(self):
+        return self.get_name_display()
+
+
+class Position(models.Model):
+    NAME_CHOICES = [
+        ('developer','Developer'),
+        ('project_manager', 'Project manager'),
+        ('qa', 'QA'),
+        ('designer', 'Designer'),
+        ('devops', 'DevOps')
+    ]
+
+    name = models.CharField(
+        max_length=255,
+        choices=NAME_CHOICES
+    )
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name='workers',
+        null = True,
+        blank = True
+    )
+
+    def __str__(self):
+        return f"{self.username} Position: {self.position}"
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('urgent', 'Urgent'),
+        ('high', 'High'),
+        ('medium', 'Medium'),
+        ('low', 'Low'),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=255,
+        choices=PRIORITY_CHOICES
+    )
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name='tasks')
+
+    def __str__(self):
+        return f"{self.name} deadline: {self.deadline} priority: {self.get_priority_display()}"
