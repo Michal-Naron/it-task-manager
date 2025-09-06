7f47527845a7e7c6161ee59d0a1fc203fc914493
Michal-Naron <michanaron@gmail.com>
2025-09-06 16:33:28 +0200

    feat(admin): register models in admin with custom list_display, filters, and unregister default Group

--- workflows/admin.py
+++ workflows/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import TaskType, Position, Worker, Task
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    model = Worker
+    list_display = UserAdmin.list_display + ('position',)
+    list_filter = UserAdmin.list_filter + ('position',)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'deadline', 'is_completed', 'priority', 'task_type')
+    list_filter = ('is_completed', 'priority', 'task_type')
+    search_fields = ('name', 'description')
+
+admin.site.unregister(Group)
