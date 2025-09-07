e052504f4598ea85e17621b6cb7b7fc8ce97efd6
Michal-Naron <michanaron@gmail.com>
2025-09-07 19:40:32 +0200

    feat(auth): create register view and add URL routing

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
+from django.views import generic
 
-from .forms import LoginForm
+from workflows.models import Worker
+from .forms import LoginForm, SignUpForm
 
 def login_view(request):
     form = LoginForm(request.POST or None)
@@ -25,3 +28,19 @@ def login_view(request):
 
     return render(request,"accounts/login.html", {"form": form, "msg": msg})
 
+
+class RegisterView(generic.CreateView):
+    model = Worker
+    form_class = SignUpForm
+    success_url = reverse_lazy("authentication:login")
+    template_name = "accounts/register.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.POST:
+            if not SignUpForm(self.request.POST).is_valid():
+                context["msg"] = "Form is not valid"
+            else:
+                context["msg"] = 'User created - please <a href="/login">login</a>.'
+
+        return context
