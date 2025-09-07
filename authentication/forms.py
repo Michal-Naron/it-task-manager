99ea9e751c529e0b709f765cc2366ec0d85b8dbd
Michal-Naron <michanaron@gmail.com>
2025-09-07 19:42:01 +0200

    fix(auth): correct SignUpForm for registration

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from workflows.models import Worker, Position
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(
@@ -16,3 +20,49 @@ class LoginForm(forms.Form):
                 "class": "form-control form-control-lg"
             }
         ))
+
+
+class SignUpForm(UserCreationForm):
+
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "username",
+                "class": "form-control form-control-lg"
+            }
+        )
+    )
+    position = forms.ModelChoiceField(
+        queryset=Position.objects.all(),
+        empty_label="Select a position",
+        widget = forms.Select(attrs={"class": "form-control form-control-lg"})
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "placeholder": "email",
+                "class": "form-control form-control-lg"
+            }
+        )
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+                "class": "form-control form-control-lg"
+            }
+        )
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder":"Repead password",
+                "class": "form-control form-control-lg"
+            }
+        )
+    )
+
+
+    class Meta:
+        model = Worker
+        fields = ("username", "position" ,"email", "password1", "password2")
