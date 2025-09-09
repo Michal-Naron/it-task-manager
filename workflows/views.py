dd06182eefefbc1114f5432846202cbbbc1ae599
Michal-Naron <michanaron@gmail.com>
2025-09-09 20:05:20 +0200

    fix(views) fix form for MyTaskViewList

--- workflows/views.py
+++ workflows/views.py
@@ -79,6 +79,11 @@ class MyTasksViewList(LoginRequiredMixin, generic.ListView):
             task.save()
         return  redirect("workflows:my-tasks-list")
 
+    def get_context_data(self, *args, object_list=None, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["search_form"] = FilterTask
+        return context
+
 
 
 class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
